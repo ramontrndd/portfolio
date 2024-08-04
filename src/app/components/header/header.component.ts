844feffe9b53cbc357bbcfe3f9317833745ada8e
ramontrndd <ramonbraintrindade@gmail.com>
2024-08-04 12:29:04 -0300

    feat: Add mobile menu functionality to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,12 +16,28 @@ import { MatToolbar } from '@angular/material/toolbar';
     CommonModule,
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  showMenu = false;
+  isMobileMenuActive = false;
 
-  toggleMenu() {
-    this.showMenu = !this.showMenu;
+  constructor(private eRef: ElementRef) {}
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuActive = !this.isMobileMenuActive;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuActive = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event): void {
+    if (
+      this.isMobileMenuActive &&
+      !this.eRef.nativeElement.contains(event.target)
+    ) {
+      this.closeMobileMenu();
+    }
   }
 }
